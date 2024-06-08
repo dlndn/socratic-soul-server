0780a858d4c2e4db0c7bd3e34f75b92d1ba5fe0f
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-08 14:08:44 -0700

    added catch-all for invalid endpoints

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
+app.all("*", (_req, res) => {
+    res.status(404).send('Status 404: Resource not found.')
+})
+
 app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`));

198fec65d7fa2265e6bdc25a7109cda86a1bb708
dlndn <65935030+dlndn@users.noreply.github.com>
2024-06-08 19:48:46 -0700

    added dot-envExpand dependency, initial chat endpoint, and role instructions for chatbot

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-require('dotenv').config();
-const PORT = process.env.PORT || 8080;
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const { GoogleGenerativeAI } = require('@google/generative-ai');
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const geminiModel = "gemini-1.5-flash";
+// .env setup
+const dotenv = require("dotenv");
+const dotenvExpand = require('dotenv-expand');
+const config = dotenv.config();
+dotenvExpand.expand(config);
 
+// express + cors setup
+const PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Gemini API setup
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const geminiModelName = "gemini-1.5-flash";
+const roleInstrEmotions = process.env.GEMINI_ROLE_INSTRUCTION_EMOTIONS;
+const roleInstrRelationships = process.env.GEMINI_ROLE_INSTRUCTION_RELATIONSHIPS;
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req, res) => {
+// routes
+app.get("/", (_req, res) => {
     res.send(`Welcome to Socratic Soul's server!`);
 });
 
-app.post('/chatbot', async (req, res) => {
-    const model = genAI.getGenerativeModel({ model: geminiModel });
+app.post("/init-chatbot", async (req, res) => {
+    const chatTopic = req.body.topic;
+
+    const model = genAI.getGenerativeModel({
+        model: geminiModelName
+    });
+    
+    const chat = model.startChat();
+    const initialMsg = (chatTopic === "Emotions") ? roleInstrEmotions : roleInstrRelationships;
+    
+    const result = await chat.sendMessage(initialMsg);
+    const response = await result.response;
+    const text = response.text();
+    res.send({
+        user: initialMsg,
+        model: text
+    });
+});
+
+app.post("/chatbot", async (req, res) => {
+    const model = genAI.getGenerativeModel({
+        model: geminiModelName
+    });
 
     const chat = model.startChat({ history: req.body.history });
     const msg = req.body.message;
@@ -27,7 +60,9 @@ app.post('/chatbot', async (req, res) => {
 });
 
 app.all("*", (_req, res) => {
-    res.status(404).send('Status 404: Resource not found.');
-})
+    res.status(404).send("Status 404: Resource not found.");
+});
 
-app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Listening on port http://localhost:${PORT}`)
+);
